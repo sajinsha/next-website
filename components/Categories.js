ea74796fb00c15d1fa26ce6af87405d4628b011c
Chrisbin Thomas <80977531+chrisbin95@users.noreply.github.com>
2023-12-24 19:12:29 +0000

    Update Categories.js

--- components/Categories.js
+++ components/Categories.js
@@ -10,13 +10,13 @@ const Categories = () => {
     <>
       <section class="text-gray-600 body-font bg-white-500">
         <div class="container px-5 py-24 mx-auto">
-          <div class="flex flex-nowrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
+          <div class="flex flex-wrap sm:-m-4 -mx-4 -mb-10 -mt-4 md:space-y-0 space-y-6">
             <div class="p-4 md:w-1/3 flex flex-col text-center items-center">
               <div>
                 <Image src={Cat4} alt="CatOne" height={275} width={261} />
               </div>
               <div class="flex-grow my-9">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                <h2 class="text-gray-800 text-lg title-font font-medium mb-3">
                   Visibility
                 </h2>
                 <p className="text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">
@@ -33,7 +33,7 @@ const Categories = () => {
                 <Image src={Cat3} alt="Cat3" height={275} width={310} />
               </div>
               <div class="flex-grow my-9">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                <h2 class="text-gray-800 text-lg title-font font-medium mb-3">
                   Real Local Agents
                 </h2>
                 <p className="text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">
@@ -53,7 +53,7 @@ const Categories = () => {
                 <Image src={Cat2} alt="Cat2" height={275} width={259} />
               </div>
               <div class="flex-grow my-9">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                <h2 class="text-gray-800 text-lg title-font font-medium mb-3">
                   Email & Notification
                 </h2>
                 <p class="text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">
@@ -75,7 +75,7 @@ const Categories = () => {
                 <Image src={Cat1} alt="Cat1" height={275} width={306} />
               </div>
               <div class="flex-grow my-9">
-                <h2 class="text-gray-900 text-lg title-font font-medium mb-3">
+                <h2 class="text-gray-800 text-lg title-font font-medium mb-3">
                   Professional Photos
                 </h2>
                 <p class="text-sm overflow-hidden whitespace-nowrap overflow-ellipsis">
