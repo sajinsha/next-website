ac45d53cd912c36869242b18f46b70536fb3fa14
Chrisbin Thomas <80977531+chrisbin95@users.noreply.github.com>
2023-12-25 15:21:43 +0000

    Update HeroSection.js
    Files from fork

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -1,34 +1,33 @@
 import Image from "next/image";
-import BannerImage from "./BannerImage.jpg";
+import BannerImage from "./Banner.png";
 
 const HeroSection = () => {
   return (
     <div className="hero bg-base-200 container-fluid">
-      <Image className="banner-img-1" src={BannerImage} alt="Banner Image" />
       <div className="hero-content text-center mt-4">
         <div className="container items-center mb-10">
-          <h1 className="text-2xl font-bold mb-6 md:mb-8 lg:mb-10 whitespace-wrap">
-            Thinking of selling your house?
+          <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-white-700 md:text-4xl lg:text-5xl dark:text-gray-700">
+          <span class="text-blue-600 dark:text-blue-500">Thinking</span> of selling your House<span class="text-blue-600 dark:text-blue-500">?</span>
           </h1>
-          <div className="text-center ml-12">
-            <p className="mr-10 text-slate-700 whitespace-wrap">
+          <div className="text-center hero-text">
+            <p className=" text-slate-700 whitespace-wrap">
               Get an instant estimate of your home's value. You can quickly
               determine the estimated value of
             </p>
             <p className=" text-slate-700 whitespace-wrap">
-              Your home by answering a few questions.
+              your home by answering a few questions.
             </p>
           </div>
 
           <div className="mt-6 md:mt-8 mb-4 mx-auto">
             <input
               type="text"
-              placeholder="Enter your address..."
+              placeholder="Enter your address.."
               className="input input-bordered input-primary w-full max-w-lg"
             />
           </div>
 
-          <button className="btn text-white bg-blue-500 rounded-full px-4 hover:bg-blue-500">
+          <button className="btn text-white bg-blue-500 rounded-full px-4 hover:bg-blue-400 hover:text-black active:text-white">
             Get Free Estimate
           </button>
         </div>
