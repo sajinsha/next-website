61e2540b17ea12d0689d1a8dd0d1d2f41e536c42
Chrisbin Thomas <80977531+chrisbin95@users.noreply.github.com>
2023-12-25 15:20:45 +0000

    Update Footer.js
    Files from fork

--- components/Footer.js
+++ components/Footer.js
@@ -2,8 +2,11 @@ import React from "react";
 
 const Footer = () => {
   return (
-  <footer class="footer-1 bg-white-500 py-8 sm:py-10">
-  <div class="container mx-auto px-4">
+  <footer class="footer-1 bg-white-500 ">
+  <div class="inline-flex items-center justify-center w-full">
+    <hr class="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700"/>
+  </div>
+  <div class="container mx-auto px-4 py-8 sm:py-10">
     <div class="sm:flex sm:flex-wrap sm:-mx-4 md:py-4">
       <div class="px-4 sm:w-1/2 md:w-1/4 xl:w-1/6">
         <h5 class="text-xl font-bold mb-6">Product</h5>
@@ -79,25 +82,30 @@ const Footer = () => {
       <div class="px-4 mt-4 mb-4 sm:w-1/3 xl:w-1/6 sm:mx-auto xl:mt-0 xl:ml-auto">
         <h5 class="text-xl font-bold mb-6 sm:text-center xl:text-left">Stay connected</h5>
         <div class="flex sm:justify-center xl:justify-start">
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-yellow-600 hover:border-yellow-600">
-            <i class="fab fa-twitter"></i>
+          <a href="https://github.com/sajinsha" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 text-gray-600 hover:text-white hover:bg-yellow-600 hover:border-yellow-600">
+          <svg class="w-5 h-5 ml-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+          <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z"/>
+          </svg>
           </a>
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-green-600 hover:border-green-400">
-            <i class="fab fa-instagram"></i>
+          <a href="https://github.com/sajinsha/next-website" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-green-600 hover:border-green-400">
+          <svg class="w-5 h-5 ml-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+          <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z"/>
+          </svg>
           </a>
-          <a href="" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-400">
-            <i class="fab fa-discord"></i>
+          <a href="https://github.com/chrisbin95" class="w-8 h-8 border border-2 border-gray-400 rounded-full text-center py-1 ml-2 text-gray-600 hover:text-white hover:bg-red-600 hover:border-red-400">
+          <svg class="w-5 h-5 ml-1 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 16">
+          <path d="M19 0H1a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V1a1 1 0 0 0-1-1ZM2 6v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V6H2Zm11 3a1 1 0 0 1-1 1H8a1 1 0 0 1-1-1V8a1 1 0 0 1 2 0h2a1 1 0 0 1 2 0v1Z"/>
+          </svg>
           </a>
         </div>
       </div>
     </div>    
   </div>
-  <div className="text-white-600 body-font bg-gray-700 p-5">
-    <p className="text-center">Copyright © <span class="text-blue-700 hover:text-blue-200">2023</span> The Canadian Home Realty Inc.</p>
+  <div className="body-font bg-gray-700 p-5">
+    <p className="text-center text-gray-200 dark:text-gray-300">Copyright © <span class="text-blue-700 hover:text-blue-200">2023</span> The Canadian Home Realty Inc.</p>
   </div>
   </footer>
   );
 };
 
 export default Footer;
-
